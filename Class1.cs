5c02fd5ab2896bcdad0eebac8ca18cfecc86c8b1
unknown <andrewduchaussee@gmail.com>
2020-02-08 21:34:51 -0500

    Created Student class and placed framework

--- Class1.cs
+++ Class1.cs
@@ -6,6 +6,33 @@ namespace WebSystemscSharp
 {
     class Student
     {
+        private int grade;//User's Grade Level
+        private String name;//User's Name
 
+        public Student(int gradeLvL, String userName)//Student Constuctor 
+        {
+            gradeLvL = grade;//set student grade level in constructor 
+            userName = name;//set student name in constructor
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+
+        }
     }
 }
+
+
+
+
+
+
