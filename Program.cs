d6b0b00e045f6b785db2de0c3229222c5327bbed
unknown <andrewduchaussee@gmail.com>
2020-02-08 20:54:47 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WebSystemscSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome();
+            
+        }
+
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to Blue Sky Tutoring");
+            Console.WriteLine("You will soon be asked to enter information about your self so we can make the perfect tutor for you");
+        }
+    }
+}
